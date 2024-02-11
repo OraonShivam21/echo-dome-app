f8703638e16869fb6e2470e8d628343906ac3605
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-02-11 16:10:49 +0530

    added database connection

--- server/server.js
+++ server/server.js
@@ -11,7 +11,20 @@ app.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to Echo Dome server api routes" });
 });
 
-const server = app.listen(
-  port,
-  console.log(`Server running on port ${port}`.yellow.bold)
-);
+const server = app.listen(port, async () => {
+  try {
+    await connection;
+    console.log(`mongodb connected`.cyan.underline);
+    console.log(`listening on port ${port}`.yellow.bold);
+  } catch (error) {
+    console.error(`Error: ${error.message}`.red.bold);
+  }
+});
+
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  },
+});
