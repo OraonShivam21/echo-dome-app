6b9f1dc98749447867a0c08d6d989681ac69ac31
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-02-11 17:33:09 +0530

    added socket connection logic

--- server/server.js
+++ server/server.js
@@ -7,17 +7,17 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
   res.status(200).json({ message: "Welcome to Echo Dome server api routes" });
 });
 
 const server = app.listen(port, async () => {
   try {
     await connection;
-    console.log(`mongodb connected`.cyan.underline);
-    console.log(`listening on port ${port}`.yellow.bold);
+    console.log(`mongodb connected`);
+    console.log(`listening on port ${port}`);
   } catch (error) {
-    console.error(`Error: ${error.message}`.red.bold);
+    console.error(`Error: ${error.message}`);
   }
 });
 
@@ -27,4 +27,39 @@ const io = require("socket.io")(server, {
     origin: process.env.CORS_ORIGIN,
     credentials: true,
   },
+});
+
+io.on("connection", (socket) => {
+  console.log("connected to socket.io");
+
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    socket.emit("connected");
+  });
+
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log(`user joined room: ${room}`);
+  });
+
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+  socket.on("new message", (newMessageReceived) => {
+    const chat = newMessageReceived.chat;
+
+    if (!chat.users) return console.log("chat.users not defined");
+
+    chat.users.forEach((user) => {
+      if (user._id === newMessageReceived.sender._id) return;
+
+      socket.in(user._id).emit("message received", newMessageReceived);
+    });
+  });
+
+  socket.off("setup", () => {
+    console.log("user disconnected");
+    socket.leave(userData._id);
+  });
 });
