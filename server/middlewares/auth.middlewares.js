d45e41e98b2842c15a3f28af0a42246e435ef6f2
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-02-11 20:38:37 +0530

    added auth middleware

---
+++ server/middlewares/auth.middlewares.js
@@ -0,0 +1,26 @@
+const asyncHandler = require("express-async-handler");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user.model");
+
+const auth = asyncHandler(async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+      const decoded = jwt.verify(token, process.env.JWT_TOKEN_SECRET);
+
+      if (decoded) throw "";
+
+      req.user = await User.findById(decoded.id).select("-password");
+      next();
+    } catch (error) {
+      res.status(401).json({ error: error || "Not authorized, token failed" });
+    }
+  }
+});
+
+module.exports = auth;
